d218df0bb8c09259516fb5df1ed7c8428e9a2ca1
garret <garret@localhost.localdomain>
2016-12-06 04:35:55 -0800

    added in the old select_track file for posterity

--- vision/angle_track.py
+++ vision/angle_track.py
@@ -1,4 +1,7 @@
 from SimpleCV import *
+from matplotlib import pyplot
+from math import degrees, pi
+from numpy import arctan2
 
 def foo(image):
     return image.meanColor()
@@ -6,17 +9,36 @@ def foo(image):
 def camshift():
     cam = Camera()
     img = cam.getImage()
+    histogram = img.hueHistogram()
+    print histogram
+    pyplot.plot(histogram)
+    pyplot.show()
+    center_point = (img.width/2, img.height/2)
+    circleLayer = DrawingLayer((img.width,img.height))
+    circleLayer.circle(center_point, 10)
     d = Display(img.size())
     bb1 = getBBFromUser(cam,d)
+    print "Bounding Box Selected"
     fs1=[]
     while True:
         try:
             img1 = cam.getImage()
-            fs1 = img1.track("camshift",fs1,img,bb1,num_frames=5, nframes=60, lower=(0, 40, 40), upper=(80, 200, 200))
+            fs1 = img1.track("camshift",fs1,img,bb1,num_frames=5, nframes=60, lower=(120, 40, 40), upper=(180, 200, 200))
+            coordset = fs1[-1]
+            angleLayer = DrawingLayer((img.width, img.height))
+            angleLayer.lines([center_point, (coordset.x, coordset.y)])
+            angleLayer.lines([(0, img.height/2), (img.width, img.height/2)])
+            angle = degrees(arctan2(coordset.x - center_point[0], coordset.y - center_point[1]) - pi/2.0) % 360
+            
             fs1.drawBB()
-            fs1.drawPath()
+            #fs1.drawPath()
+            fs1.draw()
             fs1.showCoordinates()
             fs1.showSizeRatio()
+            img1.addDrawingLayer(circleLayer)
+            img1.addDrawingLayer(angleLayer)
+            img1.applyLayers()
+            img1.drawText("Angle: " + str(angle))
             img1.show()
         except KeyboardInterrupt:
             print "\nTracked frames: ",
