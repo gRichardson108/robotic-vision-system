5de23cdc48eefd4b3b5090cc3cc5c289db8bcce4
manvelmk <manvelmkhitaryan@yahoo.com>
2016-12-05 21:42:16 -0800

    implemented custom coordinate input from serial
    It takes a little long for each value to register. First input coordinate for X when prompted (smaller numbers, can be decimal), then when prompted for Y input the second coordinate (4 was my main testing point).

--- servo_coordinate.ino
+++ servo_coordinate.ino
@@ -5,7 +5,9 @@ Servo servo;
 
 const float pi = 3.14159265359;
 
+
 float ObjX, ObjY;    // object coordinates
+float SerX, SerY, junk;
 float MotorX, MotorY; // motor coordinates
 float deltaX, deltaY; // coordinate difference
 float angleRad, angleDeg;
@@ -61,5 +63,31 @@ void loop() {
   motorTurn(4,4);
   delay(1000);
   motorTurn(5,4);
+  delay(1000);
+  
+  
+  // manual coordinates
+  Serial.print("\nCustom Coordinate X\n");
+  while (Serial.available() == 0) ;  // Wait here until input buffer has a character
+  {
+      //Side 1
+    SerX = Serial.parseFloat();        // new command in 1.0 forward
+    Serial.print("X = "); 
+    Serial.println(SerX, DEC);
+    while (Serial.available() > 0)  // .parseFloat() can leave non-numeric characters
+    { junk = Serial.read() ; }      // clear the keyboard buffer
+  }
+  Serial.print("\nCustom Coordinate Y\n");
+  while (Serial.available() == 0) ;
+  {
+      //Side 2
+    SerY = Serial.parseFloat();
+    Serial.print("Y = "); 
+    Serial.println(SerY, DEC);
+    while (Serial.available() > 0)
+    { junk = Serial.read() ; }
+  }
+  
+  motorTurn(SerX, SerY);
   delay(1000);
 }
