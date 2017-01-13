322f0c94bd994b3452accf08f9d0619b9b775ebc
haquem1 <maroof.haque@gmail.com>
2017-01-12 21:49:45 -0800

    ROVI-36

--- Arduino/sketchbook/libraries/AccelStepper/motorState.py
+++
@@ -1,7 +0,0 @@
-from nanpy import ArduinoApi
-from nanpy import SerialManager
-from nanpy import Stepper
-
-#Establish connection
-connection = SerialManager()
-a = ArduinoApi(connection = connection)
