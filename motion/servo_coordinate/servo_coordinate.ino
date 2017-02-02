fc303e4c1910294cf949cb0c0e1d85c82e88af7b
Manvel Mkhitaryan <Manvel Mkhitaryan>
2017-02-01 21:07:28 -0800

    Removed angle calculations. Now it takes TWO values in serial, with the same <X,Y> format and sets angles for each servo.

--- motion/servo_coordinate/servo_coordinate.ino
+++ motion/servo_coordinate/servo_coordinate.ino
@@ -1,54 +1,66 @@
 #include <Servo.h>
 #include <math.h>
 
-#define SERVO_MIN 5
-#define SERVO_CALIBRATE (-25)
-#define SERVO_MAX 175
+#define SERVO_MIN 0
+#define SERVO_CALIBRATE (-2)
+#define SERVO_MAX 173
 
-#define clamp(x,mn,mx) ((x<mn)?(mn):(x>mx)?(mx):(x))
+//#define clamp(x,mn,mx) ((x<mn)?(mn):(x>mx)?(mx):(x))
 
 Servo *twist;
 Servo *arm;
 
-
-float MotorX, MotorY; // motor coordinates
+//float MotorX, MotorY; // motor coordinates
 void serialTurn(Servo*);
-void pointOnPlain(Servo*, float, float);
-bool parseMessage(float output[3]);// retrieve <X,Y,Theta> coordinates
-void moveAll(float x, float y, float theta);
-float message[3];
+//void pointOnPlain(Servo*, float, float);
+bool parseMessage(float output[2]);// retrieve <horizontal,vertical> angles
+void moveAll(float angle, float theta);
+float message[2];
 
 void setup() {
 	Serial.begin(9600);
+ 
 	twist = new Servo();
 	twist->attach(9);
 
 	arm = new Servo();
 	arm->attach(10);
 
+/*
 	// define motor position
 	MotorX = 0;
 	MotorY = 0;
+*/
 
 	message[0] = 0;
-	message[1] = 1;
-	message[2] = 120;
+	message[1] = 120;
 
-	arm->write(120);
+  twist->write(90);
+	arm->write(90);
 }
 
 void loop() {
 	if (!parseMessage(message)) {
-		moveAll(message[0], message[1], 120);
-		int output = twist->read() - SERVO_CALIBRATE;
+		moveAll(message[0], message[1]);
+		int output = twist->read();
+		Serial.println(message[0]);
+		Serial.println(message[1]);
 		Serial.println(output);
+		Serial.println();
+		delay(250);
 	}
 }
 
-void moveAll(float x, float y, float theta) {
+void moveAll(float arm_angle, float theta) {
+  if ((arm_angle > SERVO_MAX) && (arm_angle <= 180)) 
+    arm_angle = SERVO_MAX;
+  else if ((arm_angle >= 0) && (arm_angle < SERVO_MIN)) 
+    arm_angle = SERVO_MIN;
+
+	twist->write(arm_angle);
 	arm->write(theta);
-	pointOnPlain(twist, x, y);
 }
+
 
 void serialTurn(Servo *servo)
 {
@@ -79,10 +91,10 @@ void serialTurn(Servo *servo)
 		}
 	}
 
-	pointOnPlain(servo, SerX, SerY);
-
+//	pointOnPlain(servo, SerX, SerY);
 }
 
+/*
 void pointOnPlain(Servo* servo, float ObjX, float ObjY) {
 	bool negative = false;
 	float deltaX = ObjX - MotorX;
@@ -108,8 +120,9 @@ void pointOnPlain(Servo* servo, float ObjX, float ObjY) {
 	servo->write(clamp(angleDeg, SERVO_MIN, SERVO_MAX));
 	delay(400);
 }
+*/
 
-bool parseMessage(float output[3]) {
+bool parseMessage(float output[2]) {
 	char c = 0;
 	if (Serial.available() <= 0) {
 		return false;
@@ -120,7 +133,7 @@ bool parseMessage(float output[3]) {
 		if (c != '<') {
 			return false;
 		}
-		for (int i = 0; i < /*3*/ 2; i++) {
+		for (int i = 0; i < 1; i++) {
 			output[i] = Serial.parseFloat();
 		}
 		return true;
