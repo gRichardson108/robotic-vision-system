0b42e5c371e8cee5ba79a53a30880de8c772c979
garret <garret@localhost.gdrlaptop>
2017-03-21 11:51:33 -0700

    first attempt - any input over serial activates emergency stop, unless it is a '0' character, which causes it to deactivate emergency stop

--- arduino/stepper_limitswitches/stepper_limitswitches.ino
+++ arduino/stepper_limitswitches/stepper_limitswitches.ino
@@ -10,59 +10,68 @@
 #define Limit02 22  // Pin 32 connected to Limit switch out
 
 int step_speed = 1;  // Speed of Stepper motor (higher = slower)
-
+int emergency_stop = 0; // when not 0, all motors must be stopped
+char received_char;
 void setup() {
-   pinMode(dir_pin, OUTPUT);
-   pinMode(step_pin, OUTPUT);
-   
-   pinMode(Limit01, INPUT);
-   pinMode(Limit02, INPUT);
-   
-   pinMode(Joy_sw01, INPUT_PULLUP);
-   
+  pinMode(dir_pin, OUTPUT);
+  pinMode(step_pin, OUTPUT);
+
+  pinMode(Limit01, INPUT);
+  pinMode(Limit02, INPUT);
+
+  pinMode(Joy_sw01, INPUT_PULLUP);
+
 
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    emergency_stop = 1;
+    received_char = Serial.read();
+    Serial.print(received_char);
+    if (received_char == '0'){
+      emergency_stop = 0;
+    }
+  }
   if (!digitalRead(Joy_sw01)) {  //  If Joystick switch is clicked
     delay(500);  // delay for deboucing
     switch (step_speed) {  // check current value of step_speed and change it
       case 10:
-        step_speed=1;  // fast speed
+        step_speed = 1; // fast speed
         break;
       case 3:
-        step_speed=10;  // slow speed
+        step_speed = 10; // slow speed
         break;
       case 1:
-        step_speed=3;  // medium speed
+        step_speed = 3; // medium speed
         break;
     }
-  }    
-    
-  if (analogRead(Joy_01) > 540) {  //  If joystick is moved Left 712
-    if (!digitalRead(Limit01)) {}  // check if limit switch is activated
-    
-      else {  //  if limit switch is not activated, move motor clockwise
-      
-        digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
-        digitalWrite(step_pin, HIGH);
-        delay(step_speed);
-        digitalWrite(step_pin, LOW);
-        delay(step_speed);
-    }      
   }
-  
-    if (analogRead(Joy_01) < 490) {  // If joystick is moved right 312
-    
-    if (!digitalRead(Limit02)) {}  // check if limit switch is activated
-    
-      else {  //  if limit switch is not activated, move motor counter clockwise
-      
-        digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
-        digitalWrite(step_pin, HIGH);
-        delay(step_speed);
-        digitalWrite(step_pin, LOW);
-        delay(step_speed);
-    }      
+
+  if (analogRead(Joy_01) > 540) {  //  If joystick is moved Left 712
+    if (!digitalRead(Limit01) && emergency_stop == 0) {}  // check if limit switch is activated
+
+    else {  //  if limit switch is not activated, move motor clockwise
+
+      digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
+      digitalWrite(step_pin, HIGH);
+      delay(step_speed);
+      digitalWrite(step_pin, LOW);
+      delay(step_speed);
+    }
+  }
+
+  if (analogRead(Joy_01) < 490) {  // If joystick is moved right 312
+
+    if (!digitalRead(Limit02)  && emergency_stop == 0) {}  // check if limit switch is activated
+
+    else {  //  if limit switch is not activated, move motor counter clockwise
+
+      digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
+      digitalWrite(step_pin, HIGH);
+      delay(step_speed);
+      digitalWrite(step_pin, LOW);
+      delay(step_speed);
+    }
   }
 }
