690ff9ca5b77115ff4e7b70d94a3f82aae3abfb7
garret <garret@localhost.gdrlaptop>
2017-04-12 17:01:33 -0700

    frame difference side by side with background sub

--- vision/backgroundSubtraction/opencvPythonTutorial/backgroundSubtractor.py
+++ vision/backgroundSubtraction/opencvPythonTutorial/backgroundSubtractor.py
@@ -1,17 +1,31 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('../testData/vtest.avi')
+cap = cv2.VideoCapture(1)
 kernel = np.ones((2,2), np.uint8)
 fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=True)
+fgbg.setHistory(30)
+blobDetector = cv2.SimpleBlobDetector_create()
+
+ret, current_frame = cap.read()
+previous_frame = current_frame
+
 while(1):
-    ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    cv2.imshow('regular',frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=4)
-    cv2.imshow('frame',fgmask)
+    current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+    previous_frame_gray = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)
+    frame_diff = cv2.absdiff(current_frame_gray, previous_frame_gray)
+    cv2.imshow('frame diff', frame_diff)
+
+    fgmask = fgbg.apply(current_frame)
+    cv2.imshow('regular',current_frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=1)
+    cv2.imshow('fgmask',fgmask)
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+
+    previous_frame = current_frame.copy()
+    ret, current_frame = cap.read()
 cap.release()
 cv2.destroyAllWindows()
