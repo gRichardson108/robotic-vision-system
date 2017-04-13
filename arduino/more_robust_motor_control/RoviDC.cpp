7e5f25054cbf1189e80f8985ecbafd6f89c85100
Daniel Schetritt <daniel.schetritt@live.com>
2017-04-12 21:56:26 -0700

    End effector works

--- arduino/more_robust_motor_control/RoviDC.cpp
+++ arduino/more_robust_motor_control/RoviDC.cpp
@@ -11,47 +11,79 @@ private:
   int dirPinB;
 
 public:
+  static const int ANTI_OVERFLOW_OFFSET = 1000;
   static const bool CLOCKWISE = true;
 
   RoviDC (int pwm_p, int dir_pin_a, int dir_pin_b)
   {
+    pulseCount = 0;
     pwmPin = pwm_p;
     dirPinA = dir_pin_a;
     dirPinB = dir_pin_b;
     pinMode(pwmPin, OUTPUT);
     pinMode(dirPinA, OUTPUT);
     pinMode(dirPinB, OUTPUT);
+    currentMillis = 0;
+    millisBetweenSteps = 1000; //default 1 step per second.
+  }
+  void sync(unsigned long currTime)
+  {
+    currentMillis = currTime;
+  }
+
+  void resetPulseCount() {
+    pulseCount = 0;
+  }
+
+  long getPulseCount() {
+    return pulseCount;
   }
 
   void setDirection(bool dir) {
-    digitalWrite(dirPinA, dir);
-    digitalWrite(dirPinB, !dir);
+    digitalWrite(dirPinA,dir);
+    digitalWrite(dirPinB,dir);
   }
 
-  void run(short velocity)
+  void setVelocity(short vel)
   {
 
-    if (vel > 0)
+    if (stepsPerSecond > 0)
     {
-      if(vel>255) {
-        vel = 255
-      }
-      setDirection(CLOCKWISE)
+      digitalWrite(directionPin,CLOCKWISE);
+      millisBetweenSteps = 1000 / stepsPerSecond;
     }
     else if(stepsPerSecond < 0)
     {
-      if(vel < -255) {
-        vel = -255
-      }
-      vel *= -1;
-      setDirection(!CLOCKWISE)
-    }
-    else {
-      digitalWrite(dirPinA,false);
-      digitalWrite(dirpinB,false);
+      digitalWrite(directionPin, !CLOCKWISE);
+      millisBetweenSteps = (-1000) / stepsPerSecond;
     }
 
-    analogWrite(pwmPin, (byte)vel)
+  }
+
+  void step()
+  {
+    if (currentMillis - previousStepMillis >= millisBetweenSteps) {
+      previousStepMillis += millisBetweenSteps;
+      digitalWrite(stepPin, HIGH);
+      digitalWrite(stepPin, LOW);
+    }
+  }
+  //returns true iff we are at the step goal.
+  bool stepTo(long stepGoal) {
+    if (stepCount < stepGoal) {
+      digitalWrite(directionPin, CLOCKWISE);
+      step();
+      return false;
+    }
+    else if(stepCount > stepGoal)
+    {
+      digitalWrite(directionPin, !CLOCKWISE);
+      step();
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
 
