c40d2777b92e0b6899e500ec6548a88ac7a465eb
garret <garret@localhost.gdrlaptop>
2017-04-12 19:10:25 -0700

    Do some morphological erosion/dialation to eliminate noise

---
+++ arduino/stepper_limitswitches/stepper_limitswitches.ino
@@ -0,0 +1,77 @@
+#define step_pin 3  // Pin 9 connected to Steps pin on EasyDriver
+#define dir_pin 2  // Pin 8 connected to Direction pin
+
+#define Joy_01 A3    // Pin A3 connected to joystick x axis
+#define Joy_sw01 11  // Pin 11 connected to joystick switch
+#define Joy_sw02 12  // Pin 11 connected to joystick switch
+
+
+#define Limit01 23  // Pin 23 connected to Limit switch out
+#define Limit02 22  // Pin 32 connected to Limit switch out
+
+int step_speed = 1;  // Speed of Stepper motor (higher = slower)
+int emergency_stop = 0; // when not 0, all motors must be stopped
+char received_char;
+void setup() {
+  pinMode(dir_pin, OUTPUT);
+  pinMode(step_pin, OUTPUT);
+
+  pinMode(Limit01, INPUT);
+  pinMode(Limit02, INPUT);
+
+  pinMode(Joy_sw01, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    emergency_stop = 1;
+    received_char = Serial.read();
+    Serial.print(received_char);
+    if (received_char == '0'){
+      emergency_stop = 0;
+    }
+  }
+  if (!digitalRead(Joy_sw01)) {  //  If Joystick switch is clicked
+    delay(500);  // delay for deboucing
+    switch (step_speed) {  // check current value of step_speed and change it
+      case 10:
+        step_speed = 1; // fast speed
+        break;
+      case 3:
+        step_speed = 10; // slow speed
+        break;
+      case 1:
+        step_speed = 3; // medium speed
+        break;
+    }
+  }
+
+  if (analogRead(Joy_01) > 540) {  //  If joystick is moved Left 712
+    if (!digitalRead(Limit01) || emergency_stop != 0) {}  // check if limit switch is activated
+
+    else {  //  if limit switch is not activated, move motor clockwise
+
+      digitalWrite(dir_pin, LOW);  // (HIGH = anti-clockwise / LOW = clockwise)
+      digitalWrite(step_pin, HIGH);
+      delay(step_speed);
+      digitalWrite(step_pin, LOW);
+      delay(step_speed);
+    }
+  }
+
+  if (analogRead(Joy_01) < 490) {  // If joystick is moved right 312
+
+    if (!digitalRead(Limit02) || emergency_stop != 0) {}  // check if limit switch is activated
+
+    else {  //  if limit switch is not activated, move motor counter clockwise
+
+      digitalWrite(dir_pin, HIGH);  // (HIGH = anti-clockwise / LOW = clockwise)
+      digitalWrite(step_pin, HIGH);
+      delay(step_speed);
+      digitalWrite(step_pin, LOW);
+      delay(step_speed);
+    }
+  }
+}
--- vision/backgroundSubtraction/opencvPythonTutorial/backgroundSubtractor.py
+++ vision/backgroundSubtraction/opencvPythonTutorial/backgroundSubtractor.py
@@ -1,11 +1,16 @@
 import numpy as np
 import cv2
 
+history = 10
+thresh = 800
+
 cap = cv2.VideoCapture(1)
 kernel = np.ones((2,2), np.uint8)
-fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=True)
-fgbg.setHistory(30)
-blobDetector = cv2.SimpleBlobDetector_create()
+fgbg = cv2.createBackgroundSubtractorKNN(history, thresh, True)
+fgbg.setShadowValue(0)
+blobDetectorParams = cv2.SimpleBlobDetector_Params()
+detector = cv2.SimpleBlobDetector_create(blobDetectorParams)
+
 
 ret, current_frame = cap.read()
 previous_frame = current_frame
@@ -13,13 +18,21 @@ previous_frame = current_frame
 while(1):
     current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
     previous_frame_gray = cv2.cvtColor(previous_frame, cv2.COLOR_BGR2GRAY)
-    frame_diff = cv2.absdiff(current_frame_gray, previous_frame_gray)
-    cv2.imshow('frame diff', frame_diff)
 
     fgmask = fgbg.apply(current_frame)
-    cv2.imshow('regular',current_frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=1)
     cv2.imshow('fgmask',fgmask)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=4)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_DILATE, kernel, iterations=4)
+    fgmask, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow('regular',current_frame)
+    cv2.drawContours(fgmask, contours, 0, (0, 255,0), 3)
+    cv2.imshow('contours', fgmask)
+
+    #keypoints = detector.detect(fgmask)
+    #im_keypoints = np.array([])
+    #im_keypoints = cv2.drawKeypoints(fgmask, keypoints, im_keypoints,(0,0,255), cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+    #cv2.imshow("keypoints", im_keypoints)
+
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
